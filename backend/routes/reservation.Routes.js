8badf14262eef46ceca70769f97d208621ce2378
ajay1264 <kambleajay146@gmail.com>
2024-09-05 02:13:50 +0530

    created routers

--- backend/routes/reservation.Routes.js
+++ backend/routes/reservation.Routes.js
@@ -1,5 +1,6 @@
 import express from "express"
-
+import { sendReservation } from "../controllers/reservation.js";
 const router = express.Router()
 
+router.post("/send", sendReservation)
 export default router;
